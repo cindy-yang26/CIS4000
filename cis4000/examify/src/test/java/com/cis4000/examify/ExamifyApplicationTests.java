db83b7227f3db2e6e8c3fca8141e35ed3b26d48f
Cindy Yang <cindyy@seas.upenn.edu>
2024-11-06 15:59:56 -0500

    starter Spring code

---
+++ cis4000/examify/src/test/java/com/cis4000/examify/ExamifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cis4000.examify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
