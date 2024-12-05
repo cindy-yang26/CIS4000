52d7f3ab6f3763f4d66e458f9fb83259784b7ba8
Cindy Yang <cindyy@seas.upenn.edu>
2024-12-04 23:29:13 -0500

    homepage styling

--- Examify/frontend/src/pages/HomePage/HomePage.js
+++ Examify/frontend/src/pages/HomePage/HomePage.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 import Header from '../../components/Header/Header';
 import { useNavigate } from 'react-router-dom';
 import './HomePage.css';
-import { FaFolder, FaEllipsisH, FaPlus } from 'react-icons/fa';
+import { FaFolder, FaPlus } from 'react-icons/fa';
+import { FiMoreVertical, FiTrash2 } from 'react-icons/fi';
+import { TiEdit } from "react-icons/ti";
+
 
 function HomePage() {
   const [courses, setCourses] = useState(['Math 220', 'Math 221', 'Math 222', 'Math 223']);
@@ -31,36 +34,38 @@ function HomePage() {
     <div className="home-page">
       <Header />
       <div className="home-content">
+
         <div className="courses-header">
-          <h2>My Courses</h2>
+          <h1 className="courses-title">My Courses</h1>
           <button className="new-course-button">
-            <FaPlus /> New
+            <FaPlus /><span className="add-course">Add Course</span>
           </button>
         </div>
+
         <div className="courses-list">
           {courses.map((course, index) => (
-            <div key={index} className="course-card">
-              <div
-                className="course-info"
-                onClick={() => handleCourseClick(course)}
-              >
-                <FaFolder className="course-icon" />
-                <span className="course-name">{course}</span>
+            <div key={index} className="home-course-card" onClick={() => handleCourseClick(course)}>
+              <div className="home-course-info">
+                <FaFolder className="home-course-icon" />
+                <span className="home-course-name">{course}</span>
               </div>
-              <FaEllipsisH
-                className="options-icon"
+              <FiMoreVertical
+                className="home-options-icon"
                 onClick={(e) => {
                   e.stopPropagation();
                   toggleMenu(index);
                 }}
               />
               {menuVisible === index && (
-                <div className="menu">
+                <div className="home-menu">
+                  <button className="home-menu-item rename">
+                    <TiEdit /> Rename
+                  </button>
                   <button
-                    className="menu-item delete-button"
+                    className="home-menu-item delete"
                     onClick={() => handleDeleteCourse(course)}
                   >
-                    Delete Course
+                    <FiTrash2 /> Delete
                   </button>
                 </div>
               )}
