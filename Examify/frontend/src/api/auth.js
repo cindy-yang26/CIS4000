13c2600e91a705f3fc532ee68554f38e1a170731
Erik Wei <erikwei@sas.upenn.edu>
2025-01-19 17:25:16 -0500

    lol

--- Examify/frontend/src/api/auth.js
+++ Examify/frontend/src/api/auth.js
@@ -3,8 +3,12 @@ import axios from 'axios';
 const API_BASE_URL = 'http://localhost:8080/api/auth';
 
 export const login = async (username, password) => {
-  try {
-    const response = await axios.post(`${API_BASE_URL}/login`, {username, password});
+  try {   
+    const response = await axios.post(`${API_BASE_URL}/login`, {
+      username, 
+      password,
+    });
+    
     console.log(response.data);
     return response.data;
   } catch (error) {
