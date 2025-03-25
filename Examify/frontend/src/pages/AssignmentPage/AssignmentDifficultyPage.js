163169b026fefc8a9287ae798c4e3a4ea4c40c19
Cindy Yang <cindyyang@Cindys-MacBook-Air.local>
2025-03-24 22:28:50 -0400

    assignment difficulty UI

--- Examify/frontend/src/pages/AssignmentPage/AssignmentDifficultyPage.js
+++ Examify/frontend/src/pages/AssignmentPage/AssignmentDifficultyPage.js
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import { fetchAssignmentInfo, fetchAssignmentQuestions } from '../../api/assignments';
 import { MathJaxContext } from 'better-react-mathjax';
+import { FaChevronLeft } from 'react-icons/fa';
 import QuestionItem from '../../components/QuestionItem'; // Import the QuestionItem component
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import './AssignmentDifficultyPage.css'; // Ensure you import the same CSS file for consistent styling
@@ -191,71 +192,92 @@ function AssignmentDifficultyPage() {
   // Data for the bar chart
   const difficultyData = getDifficultyCounts();
 
+  const handleReturnToAssignment = () => {
+    navigate(`/course/${courseId}/assignment/${assignmentId}`);
+  }
+
   return (
     <MathJaxContext>
       <div className="assignment-page">
         <Header />
-        <div className="assignment-content">
-          <div className="header-container">
-            <div>
-              <h2>Assignment Difficulty Analysis: {assignmentName}</h2>
-              <p>Overall statistics for all questions in {assignmentName}.</p>
+        <div className="assignment-content" id="difficulty-content">
+          <div className="difficulty-header">
+            <div className="header-left">
+              <button className="back-button" onClick={handleReturnToAssignment}>
+                <FaChevronLeft />
+              </button>
+              
+              <div className="assignment-title-section">
+                <h2 className="assignment-title">Assignment Difficulty Analysis: {assignmentName.replace(/-/g, ' ')}</h2>
+              </div>
             </div>
-            <button onClick={toggleMissingStats} className="toggle-missing-stats-button">
-              {showMissingStats ? 'Hide Statistics Editor' : 'Show Statistics Editor'}
-            </button>
+            
+            <div className="assignment-actions">
+              <button
+                className="toggle-missing-stats-button"
+                onClick={toggleMissingStats}
+              >
+                {showMissingStats ? 'Hide Statistics Editor' : 'Test Different Statistic Scores'}
+              </button>
+            </div>
           </div>
 
           {/* Inputs for all statistics (collapsible dropdown) */}
           {showMissingStats && Object.keys(missingStats).length > 0 && (
             <div className="missing-stats-dropdown">
-              <h3>Edit Statistics for All Questions</h3>
+              <h3 className="estimate-title">Estimate Statistics for this Assignment</h3>
+              <h4 className="estimate-text">Test different scores here. Any changes you make will not be saved.</h4>
               {questions.map((q) => (
-                <div key={q.id} className="question-item">
+                <div key={q.id} className="question-item" id="difficulty-question-item">
                   {/* Render the question details using the QuestionItem component */}
+                  <div style={{width:"75%"}}>
                   <QuestionItem question={q} />
+                  </div>
 
                   {/* Statistics input fields */}
-                  <div className="stats-inputs">
-                    <div className="stat-input">
-                      <label>Mean:</label>
-                      <input
-                        type="number"
-                        value={missingStats[q.id]?.mean || ''}
-                        onChange={(e) => handleInputChange(q.id, 'mean', e.target.value)}
-                      />
-                    </div>
-                    <div className="stat-input">
-                      <label>Median:</label>
-                      <input
-                        type="number"
-                        value={missingStats[q.id]?.median || ''}
-                        onChange={(e) => handleInputChange(q.id, 'median', e.target.value)}
-                      />
-                    </div>
-                    <div className="stat-input">
-                      <label>Std Dev:</label>
-                      <input
-                        type="number"
-                        value={missingStats[q.id]?.stdDev || ''}
-                        onChange={(e) => handleInputChange(q.id, 'stdDev', e.target.value)}
-                      />
-                    </div>
-                    <div className="stat-input">
-                      <label>Min:</label>
-                      <input
-                        type="number"
-                        value={missingStats[q.id]?.min || ''}
-                        onChange={(e) => handleInputChange(q.id, 'min', e.target.value)}
-                      />
-                    </div>
-                    <div className="stat-input">
-                      <label>Max:</label>
-                      <input
-                        type="number"
-                        value={missingStats[q.id]?.max || ''}
-                        onChange={(e) => handleInputChange(q.id, 'max', e.target.value)}
-                      />
+                  <div className="stat-estimate-div">
+                    <h4 id="estimate-header">Estimate Statistics</h4>
+                    <div className="stats-inputs">
+                      <div className="stat-input">
+                        <label>Mean:</label>
+                        <input
+                          type="number"
+                          value={missingStats[q.id]?.mean || ''}
+                          onChange={(e) => handleInputChange(q.id, 'mean', e.target.value)}
+                        />
+                      </div>
+                      <div className="stat-input">
+                        <label>Median:</label>
+                        <input
+                          type="number"
+                          value={missingStats[q.id]?.median || ''}
+                          onChange={(e) => handleInputChange(q.id, 'median', e.target.value)}
+                        />
+                      </div>
+                      <div className="stat-input">
+                        <label>Std Dev:</label>
+                        <input
+                          type="number"
+                          value={missingStats[q.id]?.stdDev || ''}
+                          onChange={(e) => handleInputChange(q.id, 'stdDev', e.target.value)}
+                        />
+                      </div>
+                      <div className="stat-input">
+                        <label>Min:</label>
+                        <input
+                          type="number"
+                          value={missingStats[q.id]?.min || ''}
+                          onChange={(e) => handleInputChange(q.id, 'min', e.target.value)}
+                        />
+                      </div>
+                      <div className="stat-input">
+                        <label>Max:</label>
+                        <input
+                          type="number"
+                          value={missingStats[q.id]?.max || ''}
+                          onChange={(e) => handleInputChange(q.id, 'max', e.target.value)}
+                        />
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -264,60 +286,61 @@ function AssignmentDifficultyPage() {
           )}
 
           {/* New Metrics Section */}
-          <div className="metrics-container">
-            <div className="metric">
-              <strong>Number of Questions:</strong> {questions.length}
+            <div className="metrics-container" id="metrics-container-difficulty">
+              <div className="metric">
+                <strong>Number of Questions:</strong> {questions.length}
+              </div>
+              <div className="metric">
+                <strong>Topics Covered:</strong> {getUniqueTagsCount()}
+              </div>
+              <div className="metric" id="last-metric">
+                <strong> Difficulty Rating:</strong> {calculateAverageDifficulty()}
+              </div>
             </div>
-            <div className="metric">
-              <strong>Topics Covered:</strong> {getUniqueTagsCount()}
+
+            {/* Overall Statistics */}
+          <div className="overall-stats-div">
+            <div className="overall-stats-container">
+              <div className="overall-stat">
+                <span><strong>Overall Mean: </strong></span>
+                <span>{overallStats.mean}</span>
+              </div>
+              <div className="overall-stat">
+                <span><strong>Overall Median: </strong></span>
+                <span>{overallStats.median}</span>
+              </div>
+              <div className="overall-stat">
+                <span><strong>Overall Std Dev: </strong></span>
+                <span>{overallStats.stdDev}</span>
+              </div>
+              <div className="overall-stat">
+                <span><strong>Min: </strong></span>
+                <span>{overallStats.min}</span>
+              </div>
+              <div className="overall-stat">
+                <span><strong>Max: </strong></span>
+                <span>{overallStats.max}</span>
+              </div>
             </div>
-            <div className="metric">
-              <strong> Difficulty Rating:</strong> {calculateAverageDifficulty()}
+
+            {/* Bar Chart for Difficulty Distribution */}
+            <div className="difficulty-chart">
+              <h3>Difficulty Distribution</h3>
+              <ResponsiveContainer width="100%" height={300}>
+                <BarChart
+                  data={difficultyData}
+                  margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="difficulty" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="count" fill="#8884d8" />
+                </BarChart>
+              </ResponsiveContainer>
             </div>
           </div>
-
-          {/* Overall Statistics */}
-          <div className="overall-stats-container">
-            <ul>
-              <li>
-                <strong>Total Mean:</strong> {overallStats.mean}
-              </li>
-              <li>
-                <strong>Total Median:</strong> {overallStats.median}
-              </li>
-              <li>
-                <strong>Total Std Dev:</strong> {overallStats.stdDev}
-              </li>
-              <li>
-                <strong>Total Min:</strong> {overallStats.min}
-              </li>
-              <li>
-                <strong>Total Max:</strong> {overallStats.max}
-              </li>
-            </ul>
-          </div>
-
-          {/* Bar Chart for Difficulty Distribution */}
-          <div className="difficulty-chart">
-            <h3>Difficulty Distribution</h3>
-            <ResponsiveContainer width="100%" height={300}>
-              <BarChart
-                data={difficultyData}
-                margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="difficulty" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="count" fill="#8884d8" />
-              </BarChart>
-            </ResponsiveContainer>
-          </div>
-
-          <button onClick={() => navigate(`/course/${courseId}/assignment/${assignmentId}`)}>
-            Back to Assignment
-          </button>
         </div>
       </div>
     </MathJaxContext>
